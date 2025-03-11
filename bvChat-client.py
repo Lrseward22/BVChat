87fdd94dbe411c7ff396d2a57a30728a30629577
rentDan <renteriadaniele@gmail.com>
2025-03-11 13:16:35 -0500

    Second push to main

--- bvChat-client.py
+++ bvChat-client.py
@@ -1,1 +1,16 @@
 #Client
+
+#separate thread handling incoming messages
+def listen():
+    pass
+
+#connect to server - login protocol
+
+#make thread for incoming messages
+
+while True:
+    #get input from user, send to server
+
+    #until /quit from user or keyboard interrupt
+    break
+
--- bvChat-server.py
+++ bvChat-server.py
@@ -1,1 +1,49 @@
 #Server
+
+def who():
+    pass
+
+def exit():
+    pass
+
+def tell(user, message):
+    pass
+
+def motd():
+    pass
+
+def me(message):
+    pass
+
+def help():
+    pass
+
+def kick():
+    pass
+
+def ban():
+    pass
+
+def unban():
+    pass
+
+def login():
+    pass
+
+#threads for each client
+def handleclient():
+    #login protocol
+
+    while True:
+        #get input from client, handle commands
+        break
+
+    pass
+
+#16 threads handling clients
+
+while True:
+    #make a client connects - send to handleclient
+    break
+
+
