74de79eab67f4934d7cdebfb21c813832cb48ea3
Logan <lrseward22@gmail.com>
2025-03-11 21:09:35 -0500

    Added basic client-server connection

--- bvChat-client.py
+++ bvChat-client.py
@@ -1,16 +1,49 @@
+from socket import *
+import threading
+import sys
 #Client
+serverIP = sys.argv[1]
+serverPort = int(sys.argv[2])
+
+serverSock = socket(AF_INET, SOCK_STREAM)
+
+# Event that allows listening thread to be stopped early
+stop_event = threading.Event()
+
+def getLine(conn):
+    msg = b''
+    while True:
+        ch = conn.recv(1)
+        msg += ch
+        if ch == b'\n' or len(ch) == 0:
+            break
+    return msg.decode()
 
 #separate thread handling incoming messages
-def listen():
-    pass
+def listen(conn):
+    while not stop_event.is_set():
+        msg = getLine(conn)
+        print(msg)
 
 #connect to server - login protocol
 
-#make thread for incoming messages
+try:
+    serverSock.connect( (serverIP, serverPort) )
+    #make thread for incoming messages
+    threading.Thread(target=listen, args=(serverSock, ), daemon=True).start()
 
-while True:
-    #get input from user, send to server
+    while True:
+        #get input from user, send to server
+        command = input("> ") + '\n'
+        serverSock.send( command.encode() )
 
-    #until /quit from user or keyboard interrupt
-    break
+        #until /quit from user or keyboard interrupt
+        if command.strip('\n') == "quit":
+            stop_event.set()
+            serverSock.close()
+            break
 
+except KeyboardInterrupt:
+    print("Client Shutting Down...")
+    stop_event.set()
+    serverSock.close()
--- bvChat-server.py
+++ bvChat-server.py
@@ -1,4 +1,15 @@
+from socket import *
+import threading
 #Server
+
+def getLine(conn):
+    msg = b''
+    while True:
+        ch = conn.recv(1)
+        msg += ch
+        if ch == b'\n' or len(ch) == 0:
+            break
+    return msg.decode()
 
 def who():
     pass
@@ -27,23 +38,50 @@ def ban():
 def unban():
     pass
 
-def login():
-    pass
+def login(conn):
+    username = "Cartman"
+    password = "111"
+    print("Logging in...")
+    getUsername = getLine(conn).strip('\n')
+    getPassword = getLine(conn).strip('\n')
+    if username == getUsername and password == getPassword:
+        return True
+    return False
 
 #threads for each client
-def handleclient():
+def handleClient(clientConn, peerAddr):
+    print("Client Connected")
     #login protocol
+    while not login(clientConn):
+        failedLoginMsg = "Your username or password is incorrect\n"
+        clientConn.send(failedLoginMsg.encode())
+
+    print("Client logged in successfully")
 
     while True:
         #get input from client, handle commands
-        break
-
+        command = getLine(clientConn).strip('\n')
+        print(command)
+        if command == "quit":
+            break
+    clientConn.close()
     pass
 
-#16 threads handling clients
+# Initial socket setup
+serverPort = 12345
+serverSock = socket(AF_INET, SOCK_STREAM)
+serverSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+serverSock.bind( ('', serverPort) )
+serverSock.listen(16)
 
-while True:
-    #make a client connects - send to handleclient
-    break
+print("Client running...")
+print("Listening on port:", serverPort)
 
+try:
+    while True:
+        #make a client connects - send to handleclient
+        threading.Thread(target=handleClient, args=(*serverSock.accept(),), daemon=True).start()
+except KeyboardInterrupt:
+    print("\n Server Shutting down...")
+    serverSock.close()
 
